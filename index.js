b76cfa4798d9af966ca9e13b2f8da13dc17365b4
Badr Afridi <badrafridi1@gmail.com>
2022-06-06 17:40:17 +0500

    a
    a

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://ebay-testing.herokuapp.com",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
