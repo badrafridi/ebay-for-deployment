8495ebceef290c08bd03aa03912ca16c704eb161
badrafridi <badrafridi1@gmail.com>
2022-06-06 15:24:31 +0500

    a
    a

--- index.js
+++ index.js
@@ -57,10 +57,10 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
-app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(express.static(path.join(__dirname, "/frontend/build")));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '/frontend/build', 'index.html'));
 });
 
 app.listen(process.env.PORT || 4000, () => {
