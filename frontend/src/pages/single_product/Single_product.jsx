679770401371a40816f8f93aed4af79dc9333366
Badr Afridi <badrafridi1@gmail.com>
2022-06-21 17:11:15 +0500

    loading for single product

--- frontend/src/pages/single_product/Single_product.jsx
+++ frontend/src/pages/single_product/Single_product.jsx
@@ -9,6 +9,7 @@ import swal from "sweetalert";
 
 export default function Single_product() {
   const api = process.env.REACT_APP_API;
+  const [loading,setLoading] = useState(true)
 
   const { usertop, setUsertop } = useContext(UserContext);
   const { cart, setCart } = useContext(CartContext);
@@ -22,6 +23,7 @@ export default function Single_product() {
   const bid = useRef(0);
   const address = useRef();
   const [userrating, setUserrating] = useState();
+
 
   const getUserrating = (user_id) => {
     axios({
@@ -57,6 +59,9 @@ export default function Single_product() {
         if (res.data.row[0].status === "sold") {
           setSoldout(true);
         }
+        if(res.data.row[0].type === "direct") {
+          setLoading(false)
+        }
         if (res.data.row[0].type === "auction") {
           var x = res.data.row[0].end_date;
           if (new Date(x).toLocaleString() < new Date().toLocaleString()) {
@@ -81,6 +86,7 @@ export default function Single_product() {
         if (res.data.message === "here are the auction details") {
           console.log(res);
           setAuctiondetails(res.data.row[0]);
+          setLoading(false)
           var x = res.data.row[0].end_date;
           if (new Date(x).toLocaleString() < new Date().toLocaleString()) {
             console.log("auction time has passed");
@@ -168,119 +174,119 @@ export default function Single_product() {
     getProduct();
   }, []);
   return (
-    <>
-      <div className="page">
-        <div className="flexRow twoColumns">
-          <div className="flexColumn full productPageProductImageBlock">
-            <img src={product.url ? product.url : ""}></img>
-          </div>
-          <div className="flexColumn full productPageProductDetailsBlock">
-            <p style={{ fontSize: "14px" }}>
-              <b>Category: </b>
-              <Link to={"/category/" + product.category_id}>
-                {product.category_name}
-              </Link>
-            </p>
-            <h3 style={{ fontSize: "34px" }}>{product.name}</h3>
-            <p>User average rating: {userrating}/5</p>
-            <h3 style={{ fontSize: "21px" }}>Price: {product.price}</h3>
-            <p>{product.description}</p>
-            <p>
-              <b>Buying Method: </b>
-              {product.type}
-            </p>
-            {product.type == "direct" ? (
+    <> {loading ? (<><div class="loaderContainer arc"></div></>) : (<>      <div className="page">
+    <div className="flexRow twoColumns">
+      <div className="flexColumn full productPageProductImageBlock">
+        <img src={product.url ? product.url : ""}></img>
+      </div>
+      <div className="flexColumn full productPageProductDetailsBlock">
+        <p style={{ fontSize: "14px" }}>
+          <b>Category: </b>
+          <Link to={"/category/" + product.category_id}>
+            {product.category_name}
+          </Link>
+        </p>
+        <h3 style={{ fontSize: "34px" }}>{product.name}</h3>
+        <p>User average rating: {userrating}/5</p>
+        <h3 style={{ fontSize: "21px" }}>Price: {product.price}</h3>
+        <p>{product.description}</p>
+        <p>
+          <b>Buying Method: </b>
+          {product.type}
+        </p>
+        {product.type == "direct" ? (
+          <>
+            {!usertop ? (
+              <>Please login to add product to the cart</>
+            ) : usertop.id == product.user_id ? (
+              <>You can not place an order on your own product</>
+            ) : soldout ? (
               <>
-                {!usertop ? (
-                  <>Please login to add product to the cart</>
-                ) : usertop.id == product.user_id ? (
-                  <>You can not place an order on your own product</>
-                ) : soldout ? (
-                  <>
-                    <p className="red">Sorry this product is sold out</p>
-                  </>
-                ) : (
-                  <input
-                    type="submit"
-                    value="Add to Cart"
-                    className="btn btn-primary btn-block mb-4"
-                    onClick={(e) => {
-                      addtocart(e);
-                    }}
-                  ></input>
-                )}
+                <p className="red">Sorry this product is sold out</p>
+              </>
+            ) : (
+              <input
+                type="submit"
+                value="Add to Cart"
+                className="btn btn-primary btn-block mb-4"
+                onClick={(e) => {
+                  addtocart(e);
+                }}
+              ></input>
+            )}
 
-                <p
-                  className="error"
-                  style={{ color: "red", marginTop: "10px" }}
-                >
-                  {error}
+            <p
+              className="error"
+              style={{ color: "red", marginTop: "10px" }}
+            >
+              {error}
+            </p>
+          </>
+        ) : (
+          <>
+            {timepassed ? (
+              <>
+                <p className="red">
+                  Sorry, auction time has ended on{" "}
+                  {new Date(auctiondetails.end_date).toLocaleString()}
                 </p>
               </>
             ) : (
               <>
-                {timepassed ? (
-                  <>
-                    <p className="red">
-                      Sorry, auction time has ended on{" "}
-                      {new Date(auctiondetails.end_date).toLocaleString()}
-                    </p>
-                  </>
-                ) : (
-                  <>
-                    <b>Last bid: </b>
-                    {auctiondetails.last_bid}
-                    <br />
-                    <b>Bid ending time: </b>
-                    {new Date(auctiondetails.end_date).toLocaleString()}
-                    <br />
-                    <b>Last bidder name: </b>
-                    {auctiondetails.username}
-                    <br />
-                    <div
-                      className="form-outline mb-4"
-                      style={{ marginTop: "30px" }}
-                    >
-                      <input
-                        type="number"
-                        id="bid"
-                        placeholder="Your Bid"
-                        className="form-control"
-                        ref={bid}
-                      ></input>
-                    </div>
-                    <div
-                      className="form-outline mb-4"
-                      style={{ marginTop: "30px" }}
-                    >
-                      <textarea
-                        id="address"
-                        placeholder="Your Address"
-                        className="form-control"
-                        ref={address}
-                      ></textarea>
-                    </div>
-                    <div className="mb-4">
-                      <input
-                        style={{ width: "100%" }}
-                        type="submit"
-                        value="Place Bid"
-                        className="btn btn-primary btn-block mb-4"
-                        onClick={() => {
-                          placeBid();
-                        }}
-                      ></input>
-                      <p className="red">{error}</p>
-                    </div>
-                  </>
-                )}
+                <b>Last bid: </b>
+                {auctiondetails.last_bid}
+                <br />
+                <b>Bid ending time: </b>
+                {new Date(auctiondetails.end_date).toLocaleString()}
+                <br />
+                <b>Last bidder name: </b>
+                {auctiondetails.username}
+                <br />
+                <div
+                  className="form-outline mb-4"
+                  style={{ marginTop: "30px" }}
+                >
+                  <input
+                    type="number"
+                    id="bid"
+                    placeholder="Your Bid"
+                    className="form-control"
+                    ref={bid}
+                  ></input>
+                </div>
+                <div
+                  className="form-outline mb-4"
+                  style={{ marginTop: "30px" }}
+                >
+                  <textarea
+                    id="address"
+                    placeholder="Your Address"
+                    className="form-control"
+                    ref={address}
+                  ></textarea>
+                </div>
+                <div className="mb-4">
+                  <input
+                    style={{ width: "100%" }}
+                    type="submit"
+                    value="Place Bid"
+                    className="btn btn-primary btn-block mb-4"
+                    onClick={() => {
+                      placeBid();
+                    }}
+                  ></input>
+                  <p className="red">{error}</p>
+                </div>
               </>
             )}
-          </div>
-        </div>
-        <div className="flexRow twoColumns"></div>
-        <div className="flexRow twoColumns"></div>
+          </>
+        )}
       </div>
+    </div>
+    <div className="flexRow twoColumns"></div>
+    <div className="flexRow twoColumns"></div>
+  </div></>)}
+
     </>
   );
 }
